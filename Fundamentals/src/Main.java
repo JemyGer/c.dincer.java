2a709f70d6850f6eca683c9386be66f35d36eb30
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-08-19 18:32:02 +0200

    add
    Main - HalloWelt
    Kreisberechnung  1 u. 2
    initalisiere den compiler via terminal

--- Fundamentals/src/Main.java
+++ Fundamentals/src/Main.java
@@ -2,11 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            // While Loop
-                int number = 1;
-                while(number <= 50){
-                    System.out.println(number);
-                    number += 1;
-                }
+                System.out.println("Der Parameter war: " + args[0]);
             }
         }
