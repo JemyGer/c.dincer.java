2fe14e9792fc6298ea29691f3d67f0aece413a9f
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-08-19 18:34:41 +0200

    fix
    -cleanCode
    better reading the radius.

--- Fundamentals/src/Kreisberechnung.java
+++ Fundamentals/src/Kreisberechnung.java
@@ -4,7 +4,7 @@
 public class Kreisberechnung {
     public static void main (String[] args) {
         double radius = 5.0;
-        double umfang = 2.0 * 3.145926 *radius;
+        double umfang = 2.0 * 3.145926 * radius;
         double inhalt = 3.145926 * radius * radius;
         System.out.println("Umfang: " + umfang);
         System.out.println("Flaeche: " +inhalt);
