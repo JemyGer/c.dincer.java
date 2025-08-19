d979b60e3e0be15d547047f3b588b055a34243cc
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-08-19 19:19:38 +0200

    add
    new
    kreisberechnung3
     - kreisberechnung mit einheiten 5 cm

---
+++ Fundamentals/src/Kreisberechnung3.java
@@ -0,0 +1,17 @@
+/*
+* Kreisberechnung: Für einen Kreis
+* werden der Umfang und der Flächeninhalt berechnet.
+* Der Radius wird beim Programmstart als erster Parameter und
+* die Einheit wird als zweiter Parameter übergeben
+* */
+public class Kreisberechnung3 {
+    public static void main (String[] args){
+        String einheit = args[1];
+        double radius = Double.parseDouble(args[0]);
+        double umfang = 2.0 * 3.1415926 * radius;
+        double flaeche = 3.1415926 * radius * radius;
+        System.out.println("Umfang: " + umfang + einheit + " ");
+        System.out.print("Fläche: " + flaeche + einheit);
+
+    }
+}
