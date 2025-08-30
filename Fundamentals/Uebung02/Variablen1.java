678232c3e9a3b6e94adc248c71f5184b7bbfcf4f
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-08-30 16:20:52 +0200

    add Uebung02 - Variablen1
    + Uebung02 - Variablen 1
    Ger:
    Tabelle erstellt mit Escape-Squencen
    Eng:
    Table created with escape sequences

---
+++ Fundamentals/Uebung02/Variablen1.java
@@ -0,0 +1,23 @@
+public class Variablen1{
+    public static void main(String[] args){
+        byte bNumber = 28;
+        short sNumber = -18453;
+        int iNumber = 4356576;
+         long lNumber = 345236577;
+        float fNumber = 4.37456678F;
+        double dNumber = 3645.564782D;
+        boolean Success = true;
+       char String = '%';
+        System.out.print("Variable\tWert");
+        System.out.print("\n bZahl" + "\t" + "\t" + bNumber);
+        System.out.print("\n sZahl" + "\t" + "\t" + sNumber);
+        System.out.print("\n iZahl" + "\t" + "\t" + iNumber);
+        System.out.print("\n lZahl" + "\t" + "\t" + lNumber);
+        System.out.print("\n fZahl" + "\t" + "\t" + fNumber);
+        System.out.print("\n dZahl" + "\t" + "\t" + dNumber);
+        System.out.print("\n bestanden" + "\t" + Success);
+        System.out.print("\n zeichen"  + "\t" + String);
+
+
+    }
+}
