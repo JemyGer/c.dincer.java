29f496235ae16e6202ee75d888daa7c429ebf204
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-09-02 08:32:15 +0200

    SimpleTable
    finished SimpleTable ( swing )
    implemented swing and expand a third row

--- Fundamentals/WithoutBook/SimpleTable.java
+++ Fundamentals/WithoutBook/SimpleTable.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
 public class SimpleTable {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        JFrame frame = new JFrame ("SimpleTable");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,400);
+        String[][] data = {
+                {"bZahl","28", "byte"},
+                {"sZahl","-18453", "short"},
+                {"iZahl", "4356576", "int"},
+                {"lZahl", "345236577970", "long"},
+                {"fZahl", "4.37456678", "float"},
+                {"dZahl", "3645.564782", "double"},
+                {"bestanden", "true", "boolean"},
+                {"zeichen", "% use single ('') for", "char"}
+        };
+        String[] columnNames = {"Variable", "Wert", "Datentypen"};
+        JTable table = new JTable(data, columnNames);
+        JScrollPane scrollPane = new JScrollPane(table);
+        frame.add(scrollPane);
+        frame.setVisible(true);
+
     }
 }
