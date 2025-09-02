6971696dca96815df7810851ada3e8bb2deda15a
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-09-02 09:25:02 +0200

    New
    Variablen2
    diffrent ways to make a Char in Java

---
+++ Fundamentals/Uebung02/Variablen2.java
@@ -0,0 +1,15 @@
+public class Variablen2 {
+    public static void main(String[] args) {
+        char z1 = 'a';
+        char z2 = 'b';
+        char z3 = 'A';
+        char z4 = 169;
+        String z5 = "\u00BE";
+        System.out.print("Variable\t" + "Wert");
+        System.out.print("\n z1" + "\t" + "\t" + z1);
+        System.out.print("\n z2" + "\t" + "\t" + z2);
+        System.out.print("\n z3" + "\t" + "\t" + z3);
+        System.out.print("\n z4" + "\t" + "\t" + z4);
+        System.out.print("\n z5" + "\t" + "\t" + z5);
+    }
+}
