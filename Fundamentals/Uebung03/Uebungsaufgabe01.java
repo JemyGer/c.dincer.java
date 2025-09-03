ad99b1789e1e09140286165da0381418dc0cfcea
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-09-03 10:38:57 +0200

    new Uebungsaufgabe01
    Berechnungen mit Variablen und Operatoren

---
+++ Fundamentals/Uebung03/Uebungsaufgabe01.java
@@ -0,0 +1,15 @@
+public class Uebungsaufgabe01 {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 5;
+        int c = 6;
+        int int_ergebnis;
+        int_ergebnis = a * b + c;
+        System.out.println(int_ergebnis);
+        double x = 1.5;
+        double y = 2.3;
+        double double_ergebnis;
+        double_ergebnis = x / y;
+        System.out.println(double_ergebnis);
+    }
+}
