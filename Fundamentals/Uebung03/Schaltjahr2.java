51053e655a6ccac6801963841ca1ff5109b67223
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-09-14 12:05:08 +0200

    delete
    boolean isLeap;
    weil keine verwendung beidngung ist schon gegeben

--- Fundamentals/Uebung03/Schaltjahr2.java
+++ Fundamentals/Uebung03/Schaltjahr2.java
@@ -6,7 +6,6 @@ public class Schaltjahr2 {
         int year = Integer.parseInt(inputYear);
 
         //Berechnung mit dem Modulo Operator und der Verketung des Booleans mit dem || oder && Operatoren.
-        boolean isLeap;
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
             JOptionPane.showMessageDialog(null, year + " ist ein Schaltjahr");
         } else {
