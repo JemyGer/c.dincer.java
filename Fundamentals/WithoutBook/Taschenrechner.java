6c1ea5b768eab8cd03c7fe4d70a1854ccb6ef094
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-09-14 11:41:34 +0200

    new
    in WithoutBook
    Taschenrechner.java erstellt und
    mit compiled mit javac
    JFrame angelegt
    Funktioniert!

---
+++ Fundamentals/WithoutBook/Taschenrechner.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class Taschenrechner {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Taschenrechner");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+}
