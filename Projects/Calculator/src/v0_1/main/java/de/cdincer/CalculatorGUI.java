83721d667e3a02dbef89e9ff0872fc75483b6563
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-10-10 07:48:48 +0200

    add
    Display
    Button1

--- Projects/Calculator/src/v0_1/main/java/de/cdincer/CalculatorGUI.java
+++ Projects/Calculator/src/v0_1/main/java/de/cdincer/CalculatorGUI.java
@@ -9,16 +9,44 @@
 package de.cdincer;
 // imports
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Font;
+
+
 
 public class CalculatorGUI extends JFrame {
-
 
     public CalculatorGUI(){
         // Frame-Einstellung
         setTitle("Calculator v0.1");
-        setSize(300,300);
+        setSize(300,400);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        // Display des Calculator ( nicht edetierbar)
+        JTextField display = new JTextField();
+        display.setPreferredSize(new Dimension(0, 50));
+        display.setFont(new Font("Arial", Font.PLAIN, 24));
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+
+        //Panel für Buttons
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5, 5));
+        add(panel, BorderLayout.CENTER);
+
+        // Button "1"
+        JButton button1 = new JButton("1");
+        panel.add(button1);
+
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                display.setText(display.getText()+ "1");
+            }
+        });
+        // Button "2"
+        JButton button2 = new JButton("")
     }
 }
