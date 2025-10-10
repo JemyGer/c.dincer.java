6034ae3d76863a251751086ef551aad73fd6a175
JemyGer <167636081+JemyGer@users.noreply.github.com>
2025-10-10 04:44:31 +0200

    change
    structure
    deleted dot for clearifacation

--- Projects/Calculator/src/v0_1/main/java/de/cdincer/Main.java
+++ Projects/Calculator/src/v0_1/main/java/de/cdincer/Main.java
@@ -1,3 +1,6 @@
+// Der Einstieg der Java funktion für die Anwendung
+// packages die bentötigt werden
+package de.c.dincer
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
