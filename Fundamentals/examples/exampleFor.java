3bef627ddbd286e265dd7ff910a7707f731b5e5c
C.Dincer <jemydev92@gmail.com>
2026-02-15 09:34:27 +0100

    create ForLoop
    created for loop example
    what counts to 100

---
+++ Fundamentals/examples/exampleFor.java
@@ -0,0 +1,7 @@
+public class exampleFor {
+    public static void main(String[] args) {
+        for (int number = 0; number < 101; number++) {
+            System.out.println(number);
+        }
+    }
+}
