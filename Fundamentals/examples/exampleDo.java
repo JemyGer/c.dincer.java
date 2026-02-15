bbaed60c5d6d8c3803db1f5b55fa04e95443056c
C.Dincer <jemydev92@gmail.com>
2026-02-15 09:28:59 +0100

    doWhile
    create doWhile loop
    with an example what counts to 100

---
+++ Fundamentals/examples/exampleDo.java
@@ -0,0 +1,9 @@
+public class exampleDo {
+    public static void main(String[] args) {
+        int Number = 0;
+        do {
+            System.out.println(Number);
+            Number++;
+        } while (Number < 101);
+    }
+}
