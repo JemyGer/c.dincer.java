d4db5c85014775790ece737942b807392766f340
C.Dincer <jemydev92@gmail.com>
2026-02-15 09:37:03 +0100

    update exampleFor
    change operator from < to <= to count to 100

--- Fundamentals/examples/exampleFor.java
+++ Fundamentals/examples/exampleFor.java
@@ -1,6 +1,6 @@
 public class exampleFor {
     public static void main(String[] args) {
-        for (int number = 0; number < 101; number++) {
+        for (int number = 0; number <= 100; number++) {
             System.out.println(number);
         }
     }
